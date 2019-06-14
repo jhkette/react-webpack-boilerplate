dd42be1fd39a4f67c43953b062774a6b8147c3fc
khenryjoe <joseph.ketterer@gmail.com>
2019-06-14 21:58:10 +0100

    gitignore

--- src/components/container.js
+++ src/components/container.js
@@ -3,7 +3,7 @@ import webpack from '../assets/webpack.svg'
 export default function container() {
   return (
     <div class="container">
-      <h1>Hello</h1>
+      <h1 style={{"font-size": "2rem"}}>React webpack boilerplate</h1>
       <img src ={webpack} />
     </div>
   )
